522ffb2570e524bdf72158854b5464372f9a5d53
Minghao Guo <mhguo@princeton.edu>
2024-07-13 00:40:01 +0300

    add hst_complex in athena_read

--- athenakit/io/athena_read.py
+++ athenakit/io/athena_read.py
@@ -1,5 +1,6 @@
 import numpy as np
 import re
+import warnings
 
 ### Read files ###
 
@@ -15,7 +16,11 @@ def hst(filename, raw=False, *args, **kwargs):
         if len(data_names) == 0:
             raise RuntimeError('Could not parse header')
     # Read data
-    arr=np.loadtxt(filename, *args, **kwargs).T
+    try:
+        arr=np.loadtxt(filename, *args, **kwargs).T
+    except:
+        warnings.warn(f"Could not read file {filename} with np.loadtxt, trying hst_complex")
+        return hst_complex(filename, raw, *args, **kwargs)
     locs=np.ones(arr[0].shape,dtype=bool)
     if (not raw):
         # Make time monotonic increasing
@@ -24,6 +29,34 @@ def hst(filename, raw=False, *args, **kwargs):
     # Make dictionary of results
     for i,name in enumerate(data_names):
         data[name]=arr[i][locs]
+    return data
+
+# Read .hst files and return dict of 1D arrays.
+def hst_complex(filename, raw=False, *args, **kwargs):
+    data = {}
+    data['time'] = []
+    with open(filename, 'r') as data_file:
+        lines=data_file.readlines()
+        for line in lines:
+            if line.startswith('#  '):
+                data_names = re.findall(r'\[\d+\]=(\S+)', line)
+                for i,name in enumerate(data_names):
+                    if name not in data:
+                        data[name]=[0.0]*len(data['time'])
+            # line is a series of numbers, convert to list of floats
+            elif line.startswith(' '):
+                data_line = [float(x) for x in line.split()]
+                for i,name in enumerate(data_names):
+                    data[name].append(data_line[i])
+    # convert lists to numpy arrays
+    for key in data:
+        data[key] = np.array(data[key])
+    if (not raw):
+        # Make time monotonic increasing
+        mono=np.minimum.accumulate(data['time'][::-1])[::-1]
+        locs=np.append(mono[:-1]<mono[1:],True)
+        for key in data:
+            data[key]=data[key][locs]
     return data
 
 # Read .tab files and return dict.
